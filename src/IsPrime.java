9e744d8c152d1072587e338483cba14339b0b2bd
Daniela Coronel <daniela.gonzaga@gmail.com>
2024-06-09 18:58:52 -0300

    isPrime

---
+++ src/IsPrime.java
@@ -0,0 +1,23 @@
+public class IsPrime {
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(2));
+        System.out.println(isPrime(7));
+        System.out.println(isPrime(10));
+
+    }
+    
+
+    public static boolean isPrime(int number) {
+        if (number < 2) {
+           return false;
+        }
+        for (int i=2; i<=Math.sqrt(number); i++) {
+          if (number % i == 0) {
+            return false;
+          }
+        }
+        return true;
+      }
+    
+}
