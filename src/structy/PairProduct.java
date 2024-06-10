c5100dc41c4506a7faea3f2aadcbcd02dd489c48
Daniela Coronel <daniela.gonzaga@gmail.com>
2024-06-10 19:14:16 -0300

    PairProduct

---
+++ src/structy/PairProduct.java
@@ -0,0 +1,35 @@
+package structy;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PairProduct {
+
+    public static void main(String[] args) {
+
+        System.out.println(pairProduct(List.of(3, 2, 5, 4, 1), 8));
+        System.out.println(pairProduct(List.of(3, 2, 5, 4, 1), 10));
+        System.out.println(pairProduct(List.of(4, 7, 9, 2, 5, 1), 5));
+        System.out.println(pairProduct(List.of(4, 7, 9, 2, 5, 1), 35));
+        
+    }
+
+    public static List<Integer> pairProduct(List<Integer> numbers, int target) {
+        
+        Map<Double,Integer> map = new HashMap<>();
+
+        for (int i = 0; i < numbers.size(); i++) {
+            int num = numbers.get(i);
+            double numbToFind = target / num;
+            if (map.containsKey(numbToFind)) {
+                return List.of(map.get(numbToFind), i);
+            }
+            map.put(Double.valueOf(numbers.get(i)), i);
+        }
+        
+        return List.of();
+
+    }
+    
+}
