72932c11df85c576aa281e49939f7b0dee3ac055
Daniela Coronel <daniela.gonzaga@gmail.com>
2024-06-11 17:40:00 -0300

    LinkedListValues

---
+++ src/structy/LinkedListValues.java
@@ -0,0 +1,59 @@
+package structy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LinkedListValues {
+
+    public static void main(String[] args) {
+
+        // test 1
+        Node<String> x = new Node<>("x");
+        Node<String> y = new Node<>("y");
+        x.next = y;
+        System.out.println(linkedListValues(x));
+        System.out.println(linkedListValuesRecursive(x));
+
+        // test 2
+        Node<String> q = new Node<>("q");
+        System.out.println(linkedListValues(q));
+        System.out.println(linkedListValuesRecursive(x));
+    }
+
+    static class Node<T> {
+        
+        T val;
+        Node<T> next;
+
+        public Node(T val) {
+            this.val = val;
+            this.next = null;
+        }
+
+    }
+
+    public static List<String> linkedListValues(Node<String> head) {
+        Node<String> current = head;
+        List<String> result = new ArrayList<>();
+        while (current != null) {
+            result.add(current.val);
+            current = current.next;
+        }
+        return result;
+    }
+
+    public static List<String> linkedListValuesRecursive(Node<String> head) {
+        List<String> result = new ArrayList<>();
+        linkedListValuesRecursive(head, result);
+        return result;
+    }
+
+    public static void linkedListValuesRecursive(Node<String> head, List<String> result) {
+        if (head == null) {
+            return;
+        }
+        result.add(head.val);
+        linkedListValuesRecursive(head.next, result);
+    }
+
+}
