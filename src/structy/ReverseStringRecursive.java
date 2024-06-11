036f2e35630bb65c37e264195cfabda69b76d824
Daniela Coronel <daniela.gonzaga@gmail.com>
2024-06-11 14:10:13 -0300

    ReverseStringRecursive

---
+++ src/structy/ReverseStringRecursive.java
@@ -0,0 +1,21 @@
+package structy;
+
+public class ReverseStringRecursive {
+
+    public static void main(String[] args) {
+
+        System.out.println(reverseString("hello"));
+        
+    }
+
+    public static String reverseString(String s) {
+        
+        if (s.isEmpty()) {
+            return "";
+        }
+
+        return reverseString(s.substring(1)) + s.charAt(0);
+        
+    }
+    
+}
