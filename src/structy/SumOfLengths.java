020dfc16be6c5dc72f695feee29d54c877621ef8
Daniela Coronel <daniela.gonzaga@gmail.com>
2024-06-10 21:31:11 -0300

    SumOfLengths

---
+++ src/structy/SumOfLengths.java
@@ -0,0 +1,21 @@
+package structy;
+
+import java.util.List;
+
+public class SumOfLengths {
+
+    public static void main(String[] args) {
+
+        System.out.println(sumOfLengths(List.of("goat", "cat", "purple")));
+        System.out.println(sumOfLengths(List.of("bike", "at", "pencils", "phone")));
+        
+    }
+
+    public static int sumOfLengths(List<String> strings) {
+        if (strings.isEmpty()) {
+            return 0;
+        }
+        return strings.get(0).length() + sumOfLengths(strings.subList(1, strings.size()));
+    }
+    
+}
