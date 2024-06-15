fe2c6eb27dce361c21d678f6b81cc8166e0dced3
Daniela Coronel <daniela.gonzaga@gmail.com>
2024-06-15 20:23:22 -0300

    binary tree problems

--- src/main/java/com/dgrc/structy/binarytree/TreeIncludes.java
+++ src/main/java/com/dgrc/structy/binarytree/TreeIncludes.java
@@ -8,23 +8,50 @@ public class TreeIncludes {
     public static void main(String[] args) {
 
         // Test 1
+        // Node<String> a = new Node<>("a");
+        // Node<String> b = new Node<>("b");
+        // Node<String> c = new Node<>("c");
+        // Node<String> d = new Node<>("d");
+        // Node<String> e = new Node<>("e");
+        // Node<String> f = new Node<>("f");
+        // a.left = b;
+        // a.right = c;
+        // b.left = d;
+        // b.right = e;
+        // c.right = f;
+        // //      a
+        // //    /   \
+        // //   b     c
+        // //  / \     \
+        // // d   e     f
+        // System.out.println(treeIncludes(a, "e")); // -> true
+
+        // Test 2
         Node<String> a = new Node<>("a");
         Node<String> b = new Node<>("b");
         Node<String> c = new Node<>("c");
         Node<String> d = new Node<>("d");
         Node<String> e = new Node<>("e");
         Node<String> f = new Node<>("f");
+        Node<String> g = new Node<>("g");
+        Node<String> h = new Node<>("h");
         a.left = b;
         a.right = c;
         b.left = d;
         b.right = e;
         c.right = f;
+        e.left = g;
+        f.right = h;
+
         //      a
         //    /   \
         //   b     c
         //  / \     \
         // d   e     f
-        System.out.println(treeIncludes(a, "e")); // -> true
+        //    /       \
+        //   g         h
+
+        System.out.println(treeIncludes(a, "f")); // -> true
         
     }
 
